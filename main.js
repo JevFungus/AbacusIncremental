124f8d90b6e4f4828cfbf667cd5e350d66391ac1
JevFungus <JevFungus@outlook.com>
2025-01-09 13:29:18 -0500

    Add files via upload
    Added more abacus styles. Added another upgrade

--- main.js
+++ main.js
@@ -1,37 +1,69 @@
-const abacusLine = ['|-0000000000|<br>', // 0
-					'|0-000000000|<br>', // 1
-			  		'|00-00000000|<br>', // 2
-			 		'|000-0000000|<br>', // 3
-			 		'|0000-000000|<br>', // 4
-			 		'|00000-00000|<br>', // 5
-			 		'|000000-0000|<br>', // 6
-			 		'|0000000-000|<br>', // 7
-			 		'|00000000-00|<br>', // 8
-			 		'|000000000-0|<br>', // 9
-			 		'|0000000000-|<br>']; // 10
+const abacusLine = [// rusian abacus
+					['|-0000000000|<br>', 
+					'|0-000000000|<br>', 
+			  		'|00-00000000|<br>',
+			 		'|000-0000000|<br>', 
+			 		'|0000-000000|<br>',
+			 		'|00000-00000|<br>', 
+			 		'|000000-0000|<br>',
+			 		'|0000000-000|<br>',
+			 		'|00000000-00|<br>',
+			 		'|000000000-0|<br>',],
+				   	// chinese abacus
+					["|00000-|-00|<br>",
+					"|0000-0|-00|<br>",
+					"|000-00|-00|<br>",
+					"|00-000|-00|<br>",
+					"|0-0000|-00|<br>",
+					"|00000-|0-0|<br>",
+					"|0000-0|0-0|<br>",
+					"|000-00|0-0|<br>",
+					"|00-000|0-0|<br>",
+					"|0-0000|0-0|<br>",],
+					// japanese abacus
+					["|0000-|-0|<br>",
+					"|000-0|-0|<br>",
+					"|00-00|-0|<br>",
+					"|0-000|-0|<br>",
+					"|-0000|-0|<br>",
+					"|000-0|0-|<br>",
+					"|00-00|0-|<br>",
+					"|0-000|0-|<br>",
+					"|-0000|0-|<br>",
+					"|0000-|0-|<br>",]
+				   ]; // 10
 let abacus = '';
-let points = 0;
+let type = 0;
+let borders = ['|-----------|', '|------|---|', '|-----|--|'];
+let points = 500;
 let pointArray = [0];
 let begone = false;
 let upgrades = 0;
 let cost = 10;
+let dubs = 0;
+let dubCost = 500;
 
-document.getElementById("upButt").style.display = "none"; 
-
+document.getElementById("upButt").style.display = 'none'; 
+document.getElementById('dubButt').style.display = 'none'; 
 //starting abacus (just zeros)
 for (let i = 0; i < pointArray.length; i++) {
 	let n = pointArray[i];
-	abacus += abacusLine[n];
+	abacus += abacusLine[type][n];
 };
 document.getElementById('abacus').innerHTML = abacus;
 
 //game loop
 window.setInterval(function(){
-	if(begone){
-		incrementor(upgrades + 1)
+	if(begone && dubs < 1){
+		incrementor(upgrades + 1);
+	} else if (begone && dubs >= 1) {
+		incrementor((upgrades + 1)*((dubs+1)*2));
 	};
 	if(points >= 10){
-		document.getElementById("upButt").style.display = "block"; 
+		document.getElementById('upButt').style.display = 'block'; 
+	};
+	if(points >= 500){
+		document.getElementById('dubButt').style.display = 'block'; 
 	};
 }, 500);
 
@@ -40,11 +72,11 @@ function abacusUpdate() {
 	abacus = '';
 	for (let i = 0; i < pointArray.length; i++) {
 		let n = pointArray[i];
-		abacus += abacusLine[n];
+		abacus += abacusLine[type][n];
 	};
 	document.getElementById('abacus').innerHTML = abacus;
 	document.getElementById('points').innerHTML = points;
-}
+};
 
 function incrementor(num) {
 	points += num;
@@ -58,9 +90,32 @@ function upgrade() {
 		cost += Math.floor(cost*1.2);
 		document.getElementById('cost').innerHTML = cost;
 		abacusUpdate();
-	}
+	};
 };
 
+function double() {
+	if (dubCost <= points){
+		dubs += 1;
+		points -= dubCost;
+		dubCost += Math.floor(dubCost*1.5);
+		upgrades *= 2;
+		document.getElementById('dubCost').innerHTML = dubCost;
+		abacusUpdate();
+	};
+};
+
+function changeType(){
+	if (type < 2){
+		type += 1;
+	} else {
+		type = 0;
+	};
+	let styleName = ['Russian', 'Chinese', 'Japanese'];
+	abacusUpdate();
+	document.getElementById('styleName').innerHTML = styleName[type];
+	document.getElementById('borders').innerHTML = borders[type];
+	document.getElementById('borders2').innerHTML = borders[type];
+};
 function begin() {
 	document.getElementById("startButt").style.display = "none"; 
 	begone = true;
